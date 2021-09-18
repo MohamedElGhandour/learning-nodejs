372e9013ec7c1a90bc90fa3c8455bd0188eced97
Mohamed Elghandour <m.yahya1728@gmail.com>
2021-09-18 21:37:29 +0200

    End Notes App 2

---
+++ playground/1-json.js
@@ -0,0 +1,29 @@
+// const fs = require("fs");
+
+// const dataBuffer = fs.readFileSync("1-json.json");
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// data.name = "Mohamed";
+// data.age = 21;
+// const createJSON = JSON.stringify(data);
+// fs.writeFileSync("1-json.json", createJSON);
+
+// const dataBuffer = fs.readFileSync("1-json.json");
+// console.log(dataBuffer);
+// const dataJSON = dataBuffer.toString();
+// console.log(dataJSON);
+// const data = JSON.parse(dataJSON);
+// console.log(data);
+
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync("1-json.json", bookJSON);
+// --------------
+// console.log(bookJSON);
+
+// const bookOBJ = JSON.parse(bookJSON);
+// console.log(bookOBJ);
---
+++ playground/2-arrow-function.js
@@ -0,0 +1,34 @@
+// const square = function (x) {
+//   return x * x;
+// };
+
+// const square = (x) => {
+//   return x * x;
+// };
+
+// const square = (x) => x * x;
+
+// console.log(square(5));
+
+// const eventObj = {
+//   name: "Birthday Party",
+//   guestList() {
+//     console.log(`Guest list for ${this.name}`);
+//   },
+// };
+
+// const eventObj = {
+//   name: "Birthday Party",
+//   guestList: () => {
+//     console.log(`Guest list for ${this.name}`);
+//   },
+// };
+
+// const eventObj = {
+//   name: "Birthday Party",
+//   guestList: function () {
+//     console.log(`Guest list for ${this.name}`);
+//   },
+// };
+
+// eventObj.guestList();
